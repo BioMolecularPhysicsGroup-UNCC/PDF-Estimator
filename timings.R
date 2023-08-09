9dbbf5aac1c9e10a250e765c8c7cf39873e39853
Flarp <flurpdadurp@gmail.com>
2023-08-09 13:24:44 -0400

    Update timings file for the moment

--- timings.R
+++ timings.R
@@ -2,14 +2,19 @@ library(PDFEstimator)
 library(benchden)
 
 datas <- list(list(14,  "Matterhorn"), list(11, "Normal"), list(1, "Uniform"))
+sample_sizes <- list(1000, 10000, 100000, 1000000)
 times <- list()
 
 for (pair in datas) {
-    for (i in 1:3) {
-        print(pair[[1]])
-        x <- rberdev(10000, pair[[1]])
-        times <- append(times, list(pair, system.time(
-            stitchPDF(x)
-        )))
+    for (sample_size in sample_sizes) {
+        for (i in 1:3) {
+            print(pair[[1]])
+            x <- rberdev(sample_size, pair[[1]])
+            times <- append(times, list(pair, sample_size, system.time(
+                stitchPDF(x)
+            )))
+        }
     }
 }
+
+print(times)
